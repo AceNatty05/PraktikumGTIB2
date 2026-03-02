ec4057dbba0875e7b924d92cdcff75a1859db90c
hananafii <hanahaq04@gmail.com>
2026-03-02 23:17:26 +0700

    Keaktifan Nomor 3

--- Pertemuan 1/Keaktifan/24060124130081_Hana Nafi'atul Haq /main.cpp
+++ Pertemuan 1/Keaktifan/24060124130081_Hana Nafi'atul Haq /main.cpp
@@ -2,7 +2,7 @@
 
 // Hana Nafi'atul Haq - 24060124130081
 
-void Kotak(float x1, float y1, float x2, float y2, float r, float g, float b) {
+void Segi4(float x1, float y1, float x2, float y2, float r, float g, float b) {
     glColor3f(r, g, b);
     glRectf(x1, y1, x2, y2);
 }
@@ -10,28 +10,33 @@ void Kotak(float x1, float y1, float x2, float y2, float r, float g, float b) {
 void display(void) {
     glClear(GL_COLOR_BUFFER_BIT);
 
-    Kotak(-0.6f, -0.2f, 0.4f, 0.2f, 1.0f, 0.5f, 0.0f); //badan
-    Kotak(0.4f, -0.15f, 0.7f, 0.15f, 1.0f, 0.3f, 0.0f); //ekor
-    Kotak(-0.2f, 0.2f, 0.1f, 0.4f, 1.0f, 0.6f, 0.2f); //sirip atas
-    Kotak(-0.2f, -0.4f, 0.1f, -0.2f, 1.0f, 0.6f, 0.2f); // sirip bawah
-    Kotak(-0.45f, 0.05f, -0.35f, 0.15f, 1.0f, 1.0f, 1.0f); // mata putih
-    Kotak(-0.42f, 0.08f, -0.38f, 0.12f, 0.0f, 0.0f, 0.0f); // mata hitam
+    Segi4(-0.6f, -0.2f, 0.4f, 0.2f, 1.0f, 0.5f, 0.0f); // badan
+    Segi4(0.4f, -0.15f, 0.7f, 0.15f, 1.0f, 0.3f, 0.0f); // ekor
+    Segi4(-0.2f, 0.2f, 0.1f, 0.4f, 1.0f, 0.6f, 0.2f); // sirip atas
+    Segi4(-0.2f, -0.4f, 0.1f, -0.2f, 1.0f, 0.6f, 0.2f); // sirip bawah
+    Segi4(-0.45f, 0.05f, -0.35f, 0.15f, 1.0f, 1.0f, 1.0f); // mata putih
+    Segi4(-0.42f, 0.08f, -0.38f, 0.12f, 0.0f, 0.0f, 0.0f); // mata hitam
+    Segi4(-0.55f, -0.05f, -0.40f, 0.0f, 0.8f, 0.2f, 0.0f); // mulut
 
     glFlush();
 }
 
 void init(void) {
-    glClearColor(0.5f, 0.8f, 1.0f, 1.0f);
+    glClearColor(0.5f, 0.8f, 1.0f, 1.0f); 
+    glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
+    gluOrtho2D(-1.0, 1.0, -1.0, 1.0);
 }
 
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
     glutInitWindowSize(600, 600);
-    glutCreateWindow("Gambar Ikan Primitif :)");
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Gambar Ikan Primitif - Hana");
 
     init();
     glutDisplayFunc(display);
     glutMainLoop();
+    return 0;
 }
