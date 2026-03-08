7c27a50eae489d9a93303bde7a5fd35e70eaacc9
sssyifaa <syifaaenii04@gmail.com>
2026-03-08 22:38:05 +0700

    Update main.cpp

--- Pertemuan 1/Tugas/24060124120043_Syifa Aeni Mudrikah/Nomor1/main.cpp
+++ Pertemuan 1/Tugas/24060124120043_Syifa Aeni Mudrikah/Nomor1/main.cpp
@@ -1,1 +1,42 @@
+#include <GL/glut.h>
 
+void Garis(void) {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glPointSize(8.0f);
+    glBegin(GL_POINTS);
+    glColor3f(1.0f, 0.0f, 0.0f); 
+    glVertex3f(-0.20f, 0.20f, 0.0f); // titik kiri
+    glVertex3f(0.20f, 0.20f, 0.0f);  // titik kanan
+    glEnd();
+
+    glLineWidth(2.0f);
+    glBegin(GL_LINES);
+    glColor3f(0.0f, 1.0f, 0.0f);
+
+    // garis kiri
+    glVertex3f(-0.20f, 0.20f, 0.0f);
+    glVertex3f(0.00f, -0.20f, 0.0f);
+
+    // garis kanan
+    glVertex3f(0.20f, 0.20f, 0.0f);
+    glVertex3f(0.00f, -0.20f, 0.0f);
+
+    glEnd();
+
+    glFlush();
+}
+
+int main(int argc, char* argv[]) {
+    glutInit(&argc, argv);
+    glutInitWindowSize(640, 480);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+    glutCreateWindow("Titik dan Garis");
+
+    glutDisplayFunc(Garis);
+
+    glClearColor(1.0f, 1.0f, 0.2f, 0.2f); 
+
+    glutMainLoop();
+    return 0;
+}
