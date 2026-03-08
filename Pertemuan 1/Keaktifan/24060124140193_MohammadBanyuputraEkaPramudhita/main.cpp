bbd40eda3727984f654509b5785c3bc49f0f17c3
BirelaMP <miadetabirela@gmail.com>
2026-03-08 23:39:46 +0700

    upload tugas Birela

---
+++ Pertemuan 1/Keaktifan/24060124140193_MohammadBanyuputraEkaPramudhita/main.cpp
@@ -0,0 +1,45 @@
+#include <GL/glut.h>
+
+void SegiEmpat(void) {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glColor3f(1.0f, 0.8f, 0.6f);
+    glRectf(-0.6f, -0.6f, 0.6f, -0.3f);
+	
+    glColor3f(1.0f, 0.7f, 0.5f);
+    glRectf(-0.5f, -0.3f, 0.5f, -0.05f);
+
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glRectf(-0.45f, -0.05f, 0.45f, 0.05f);
+
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glRectf(-0.25f, 0.05f, -0.2f, 0.35f);
+
+    glColor3f(1.0f, 1.0f, 0.0f);
+    glRectf(-0.235f, 0.35f, -0.215f, 0.42f);
+
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glRectf(-0.025f, 0.05f, 0.025f, 0.35f);
+
+    glColor3f(1.0f, 1.0f, 0.0f);
+    glRectf(-0.01f, 0.35f, 0.01f, 0.42f);
+
+    glColor3f(1.0f, 0.0f, 1.0f);
+    glRectf(0.2f, 0.05f, 0.25f, 0.35f);
+
+    glColor3f(1.0f, 1.0f, 0.0f);
+    glRectf(0.215f, 0.35f, 0.235f, 0.42f);
+
+    glFlush();
+}
+
+int main(int argc, char* argv[]) {
+    glutInit(&argc, argv);
+    glutInitWindowSize(640, 480);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+    glutCreateWindow("Happy Birth Day for you!");
+    glutDisplayFunc(SegiEmpat);
+    glClearColor(1.0f, 0.6f, 0.8f, 1.0f);
+    glutMainLoop();
+    return 0;
+}
---
+++ Pertemuan 1/Tugas/24060124120002_Birela Miadeta Purita/main.cpp
@@ -0,0 +1,94 @@
+#include <GL/glut.h>
+
+// =================================================================
+// Nama: Birela Miadeta Purita
+// Lab:  B2
+// NIM:  24060124120002
+// Tujuan: Tugas 1 - Mengimplementasikan berbagai Grafik Primitif Dasar
+// =================================================================
+
+void displayPrimitif(void) {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glLoadIdentity();
+
+    // 1. GL_POINTS (Membuat Titik)
+    glPointSize(10.0f); 
+    glBegin(GL_POINTS);  
+        glColor3f(1.0f, 0.0f, 0.0f); // Warna Merah 
+        glVertex2f(-0.7, 0.7); 
+    glEnd();
+
+    // 2. GL_LINES (Membuat Garis)
+    glLineWidth(3.0f); 
+    glBegin(GL_LINES); 
+        glColor3f(1.0f, 1.0f, 1.0f); // Warna Putih 
+        glVertex2f(-0.8, 0.4);
+        glVertex2f(-0.4, 0.4);
+    glEnd();
+
+    // 3. GL_LINE_STRIP (Garis bersambung terbuka)
+    glColor3f(0.0f, 1.0f, 0.0f); // Warna Hijau 
+    glBegin(GL_LINE_STRIP); 
+        glVertex2f(0.1, 0.7);
+        glVertex2f(0.3, 0.9);
+        glVertex2f(0.5, 0.7);
+    glEnd();
+
+    // 4. GL_LINE_LOOP (Garis bersambung tertutup)
+    glColor3f(0.0f, 1.0f, 1.0f); // Warna Cyan
+    glBegin(GL_LINE_LOOP); 
+        glVertex2f(0.6, 0.7);
+        glVertex2f(0.8, 0.9);
+        glVertex2f(0.9, 0.7);
+    glEnd();
+
+    // 5. GL_TRIANGLE_FAN (Segitiga menyebar dari satu pusat)
+    glColor3f(1.0f, 1.0f, 0.0f); // Warna Kuning
+    glBegin(GL_TRIANGLE_FAN);  
+        glVertex2f(-0.6, -0.2); 
+        glVertex2f(-0.8, -0.4);
+        glVertex2f(-0.6, -0.6);
+        glVertex2f(-0.4, -0.4);
+    glEnd();
+
+    // 6. GL_TRIANGLE_STRIP (Segitiga bersambung)
+    glColor3f(1.0f, 0.0f, 1.0f); // Warna Ungu
+    glBegin(GL_TRIANGLE_STRIP); 
+        glVertex2f(0.1, -0.4);
+        glVertex2f(0.2, -0.2);
+        glVertex2f(0.3, -0.4);
+        glVertex2f(0.4, -0.2);
+    glEnd();
+
+    // 7. GL_QUADS (Segiempat)
+    glColor3f(1.0f, 0.5f, 0.0f); // Warna Oranye
+    glBegin(GL_QUADS); 
+        glVertex2f(0.6, -0.2);
+        glVertex2f(0.9, -0.2);
+        glVertex2f(0.9, -0.5);
+        glVertex2f(0.6, -0.5);
+    glEnd();
+
+    // 8. GL_QUAD_STRIP (Segiempat bersambung)
+    glColor3f(0.5f, 0.5f, 1.0f); // Warna Biru 
+    glBegin(GL_QUAD_STRIP); // 
+        glVertex2f(-0.2, -0.7);
+        glVertex2f(-0.2, -0.9);
+        glVertex2f(0.2, -0.7);
+        glVertex2f(0.2, -0.9);
+        glVertex2f(0.6, -0.7);
+        glVertex2f(0.6, -0.9);
+    glEnd();
+    glFlush(); 
+}
+
+int main(int argc, char* argv[]) {
+    glutInit(&argc, argv);
+    glutInitWindowSize(640, 480); 
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA); 
+    glutCreateWindow("Tugas 1 - Soal no 1 & 2");
+    glutDisplayFunc(displayPrimitif); 
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f); 
+    glutMainLoop();
+    return 0;
+}
