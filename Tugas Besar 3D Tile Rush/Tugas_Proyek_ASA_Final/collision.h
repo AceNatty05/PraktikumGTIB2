b0f077f64f72ab40b14c76d9d5d6c3683c11e093
diahmaulida <diahmaulida555@gmail.com>
2026-06-06 16:58:27 +0700

    Delete Tugas 3D Tile Rush directory

--- Tugas Besar 3D Tile Rush/Tugas_Proyek_ASA_Final/collision.h
+++
@@ -1,9 +0,0 @@
-#ifndef COLLISION_H
-#define COLLISION_H
-
-#include "global.h"
-
-void updateCollision();
-void triggerDeath();
-
-#endif
--- Tugas Besar 3D Tile Rush/Tugas_Proyek_ASA_Final/lighting.h
+++
@@ -1,8 +0,0 @@
-#ifndef LIGHTING_H
-#define LIGHTING_H
-
-#include "global.h"
-
-void initLighting();
-
-#endif
